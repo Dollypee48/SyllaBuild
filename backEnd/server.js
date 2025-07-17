a2be12cb77c54952f330914de70dbe89362839fe
Dollypee48 <akinolad22@gmail.com>
2025-07-17 15:22:23 +0100

    solve the server issue

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,36 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require("dotenv");
-const bodyParser = require('body-parser')
-const connectDB = require('./config/db')
-const authRoutes = require('./routes/authRoutes')
-const courseRoutes = require('./routes/courseRoutes')
-
-
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const courseRoutes = require('./routes/courseRoutes');
 
 dotenv.config();
-connectDB()
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 
-const app = express()
-const PORT = process.env.PORT || 5000
-
-/
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("📚 SyllaBuild API is running");
+app.get('/', (req, res) => {
+  res.send('📚 SyllaBuild API is running');
 });
 
 // Routes
-app.use('/api/auth', authRoutes)
-app.use('/api/courses', courseRoutes)
-
-
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
